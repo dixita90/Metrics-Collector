c790dcffe44c2aada183b764edf2063cf0c99baa
krishc90 <chintapallisiva90@gmail.com>
2016-03-20 21:21:23 -0400

    directory structure

---
+++ SySTemantics/src/main/java/com/mscs_710l/systemantics/SyStemantics.java
@@ -0,0 +1,15 @@
+package com.mscs_710l.systemantics;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SIVARAMAKRISHNAPRASA
+ */
+public class SyStemantics {
+  
+}
