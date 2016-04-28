21f4f78b84510004ee588f536371af9fd2a63c61
dixita90 <dixita.sharegar1@marist.edu>
2016-04-28 19:09:23 -0400

    Code added for converting resultset to tableview but still needs testing

--- SySTemantics/src/main/java/com/mscs_710l/systemantics/ui/SyStemantics.java
+++ SySTemantics/src/main/java/com/mscs_710l/systemantics/ui/SyStemantics.java
@@ -49,4 +49,55 @@ public class SyStemantics {
      cpuInfo.cpuInformation();
     LOGGER.debug("SySTematics main(): ends");
   }
+  
+  /*
+  public void BindTable() {
+        try {
+            CpuInfo cpuInfo = new CpuInfo();
+            cpuInfo.memoryStats(cmdFreeMemory);
+            ResultSet rs = cpuInfo.getCpuInfoResultSet();
+            /**
+             * ********************************
+             * TABLE COLUMN ADDED DYNAMICALLY * ********************************
+             */
+/*            for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                //We are using non property style for making dynamic table
+                final int j = i;
+                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+                tblProcess.getColumns().addAll(col);
+                System.out.println("Column [" + i + "] ");
+            }
+
+            /**
+             * ******************************
+             * Data added to ObservableList * ******************************
+             */
+  /*          while (rs.next()) {
+                //Iterate Row
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    //Iterate Column
+                    row.add(rs.getString(i));
+                }
+                System.out.println("Row [1] added " + row);
+                data.add(row);
+
+            }
+
+            //FINALLY ADDED TO TableView
+            tblProcess.setItems(data);
+            //FINALLY ADDED TO TableView
+            tblProcess.setItems(data);
+
+        } catch (SQLException ex) {
+            LOGGER.error("Error in BindTable() in SyStemantics.java: "+ex.getMessage());
+        }
+    }
+  */
 }
